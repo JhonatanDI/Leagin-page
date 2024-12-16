e7bfc754011069c0051a5387168b8c2cad0f934b
JhonatanDI <jhonatandinamit@gmail.com>
2024-12-15 23:41:21 -0300

    Adiçcao de conteúdo
    nesta atualização adicionei os conteúdos q aprendi durante 40 aulas

---
+++ Projeto Leagin Page/index.js
@@ -0,0 +1,27 @@
+var setadireita = window.document.getElementById("setadireita")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setaesquerda = window.document.getElementById("setaesquerda")
+
+
+function rolarparadireita(){
+
+setadireita.style = "display:none"
+leonardo.style = "display:none"
+bruna.style = "display:flex"
+setaesquerda.style = "display:flex; margin-top105%px"
+
+}
+
+function rolarparaesquerda(){
+
+setadireita.style ="diplay:flex"
+bruna.style ="display:none"
+leonardo.style = "display:flex"
+setaesquerda.style = "display:none"
+
+
+}
+
+
